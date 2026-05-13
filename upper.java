1f67d07953a94e05d8ac2c33d6cb9fb9523a14fc
Chethana <chethanasriadusumalli@gmail.com>
2026-05-13 21:52:43 +0530

    Create upper.java

---
+++ upper.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class CaseCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        int upper = 0;
+        int lower = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch >= 'A' && ch <= 'Z') {
+                upper++;
+            } else if (ch >= 'a' && ch <= 'z') {
+                lower++;
+            }
+        }
+
+        System.out.println("Uppercase letters = " + upper);
+        System.out.println("Lowercase letters = " + lower);
+    }
+}
