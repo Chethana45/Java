57258ab99489bdc170fe6a9004e1a080d8694336
Chethana <chethanasriadusumalli@gmail.com>
2026-05-18 21:10:27 +0530

    Create bubble.java

---
+++ bubble.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[5];
+
+        System.out.println("Enter 5 numbers:");
+
+        for (int i = 0; i < 5; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 5 - 1; i++) {
+            for (int j = 0; j < 5 - i - 1; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("Sorted Array:");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
