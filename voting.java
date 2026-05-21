6feb27e7153fcc1df9eebf2b2cb830ca565f2601
AdusumalliChethanaSri <chethanasriadusumalli@gmail.com>
2026-05-21 22:45:05 +0530

    Add VotingEligibility class to check voting age

---
+++ voting.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class VotingEligibility {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int age;
+
+        System.out.print("Enter your age: ");
+        age = sc.nextInt();
+
+        if (age >= 18) {
+            System.out.println("Eligible for Voting");
+        } else {
+            System.out.println("Not Eligible for Voting");
+        }
+    }
+}
